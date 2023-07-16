6f748dec79db2da3ccf74e4aea33532ff288f8cd
Sonu <RAJS64163@GMAIL.COM>
2023-07-16 14:35:49 +0530

    convert to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Screen from "./Components/Screen";
+import Button from "./Components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <Screen />
+        <div className="buttonBox">
+          {btnValues.flat().map((btn, i) => (
+            <Button value={btn} key={i} />
+          ))}
+        </div>
+        <div className="myName">&copy; BY SONU</div>
+      </div>
+    </>
   );
 }
 
